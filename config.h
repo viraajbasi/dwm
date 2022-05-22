174c5bc912a195a3f5daa7afc74bcc354352759e
Viraaj Basi <viraajbasi@gmail.com>
2022-05-22 18:47:41 +0100

    replaced dmenu with rofi

--- config.h
+++ config.h
@@ -65,10 +65,11 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", "dmenumon", NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *roficmd[] = { "rofi", "-show", "combi", "-show-icons", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
