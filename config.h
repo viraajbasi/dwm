13d7171a3832e60b53b352c9def7f845fc490b39
Viraaj Basi <viraajbasi@gmail.com>
2022-05-22 19:24:11 +0100

    added new patches

--- config.h
+++ config.h
@@ -97,6 +97,10 @@ static Key keys[] = {
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
     { MODKEY|ShiftMask,             XK_F5,     xrdb,           {.v = NULL } },
+    { MODKEY,                       XK_Left,   viewtoleft,     {0} },
+    { MODKEY,                       XK_Right,  viewtoright,    {0} },
+    { MODKEY|ShiftMask,             XK_Left,   tagtoleft,      {0} },
+    { MODKEY|ShiftMask,             XK_Right,  tagtoright,     {0} },
     TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
