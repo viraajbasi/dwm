439ca0423392134309b5176d9dff45af2dfd4b93
Viraaj Basi <viraajbasi@gmail.com>
2022-05-26 16:19:47 +0100

    added bar height patch

--- config.def.h
+++ config.def.h
@@ -14,6 +14,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
+static const int user_bh            = 25;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 #define ICONSIZE bh   /* icon size */
 #define ICONSPACING (bh - 4) /* space between icon and title */
 static const char *fonts[]          = { "Caskaydia Cove Nerd Font:size=16", "JoyPixels:pixelsize=16:antialias=true:autohint=true" };
