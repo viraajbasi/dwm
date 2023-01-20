a5e0b2e0b0f5b78798db70cd17bcb8fd8e1a8066
Viraaj Basi <viraajbasi@gmail.com>
2023-01-20 16:45:44 +0000

    changed default layout

--- config.h
+++ config.h
@@ -49,10 +49,10 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
+	{ "|||",      col },
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
-	{ "|||",      col },
 };
 
 /* key definitions */
